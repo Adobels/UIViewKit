0c158b80f1724829aadb950f1cd16be6f1056b16
Blazej SLEBODA <5544365+Adobels@users.noreply.github.com>
2023-10-05 20:18:32 +0200

    Develop disable iboutlet trailing closure (#3)
    * Unpublish iboutlet with trailing closures. Use ibsubiews instead
    
    * desactivate iboutlet variants with trailing ibsubview closures due to possible missuage in edge cases

--- Sources/UIViewKit/UIViewDSL+IBOutlet.swift
+++ Sources/UIViewKit/UIViewDSL+IBOutlet.swift
@@ -23,50 +23,8 @@ extension UIViewDSL where Self: UIView {
     }
     
     @discardableResult
-    public func ibOutlet(_ outlet: inout Self?, @UIViewBuilder _ content: () -> [UIView]) -> Self {
-        outlet = self
-        UIViewDSLEngine.shared.addSubviews(content, to: self)
-        return self
-    }
-    
-    @discardableResult
-    public func ibOutlet(_ outlet: inout Self?, @UIViewBuilder _ content: (UIView) -> [UIView]) -> Self {
-        outlet = self
-        UIViewDSLEngine.shared.addSubviews(content, to: self)
-        return self
-    }
-    
-    @discardableResult
-    public func ibOutlet(_ outlet: inout Self, @UIViewBuilder _ content: () -> [UIView]) -> Self {
-        outlet = self
-        UIViewDSLEngine.shared.addSubviews(content, to: self)
-        return self
-    }
-    
-    @discardableResult
-    public func ibOutlet(_ outlet: inout Self, @UIViewBuilder _ content: (UIView) -> [UIView]) -> Self {
-        outlet = self
-        UIViewDSLEngine.shared.addSubviews(content, to: self)
-        return self
-    }
-
-    @discardableResult
     public func ibOutlet(in outlet: inout [Self]) -> Self {
         outlet.append(self)
-        return self
-    }
-    
-    @discardableResult
-    public func ibOutlet(in outlet: inout [Self], @UIViewBuilder _ content: () -> [UIView]) -> Self {
-        outlet.append(self)
-        UIViewDSLEngine.shared.addSubviews(content, to: self)
-        return self
-    }
-    
-    @discardableResult
-    public func ibOutlet(in outlet: inout [Self], @UIViewBuilder _ content: (UIView) -> [UIView]) -> Self {
-        outlet.append(self)
-        UIViewDSLEngine.shared.addSubviews(content, to: self)
         return self
     }
 }
