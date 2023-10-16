7616592f1f8295eb68d351c8813aca9918aabbb2
Blazej SLEBODA <5544365+adobels@users.noreply.github.com>
2023-10-16 17:27:29 +0200

    Add support for Carthage

---
+++ Sources/UIViewKit/UIViewKit.h
@@ -0,0 +1,18 @@
+//
+//  UIViewKit.h
+//  UIViewKit
+//
+//  Created by MaxAir on 16/10/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIViewKit.
+FOUNDATION_EXPORT double UIViewKitVersionNumber;
+
+//! Project version string for UIViewKit.
+FOUNDATION_EXPORT const unsigned char UIViewKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIViewKit/PublicHeader.h>
+
+
