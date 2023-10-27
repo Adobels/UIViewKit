816cd319a6aa2265e58b44b387a64839e2b36a8c
Blazej SLEBODA <5544365+adobels@users.noreply.github.com>
2023-10-27 09:31:14 +0200

    Various in WIP

---
+++ Sources/UIViewKit/IBView.swift
@@ -0,0 +1,22 @@
+//
+//  IBView.swift
+//  UIViewKit
+//
+//  Created by MaxAir on 19/10/2023.
+//
+
+import UIKit
+
+open class IBView: UIView {
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        createView(frame: frame)
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    open func createView(frame: CGRect) { }
+}
