44d30e31a8579794e970f072f8cbaef05a255345
Blazej SLEBODA <5544365+adobels@users.noreply.github.com>
2023-10-29 18:24:30 +0100

    update addSubviews to be Self typed

--- Sources/UIViewKit/UIViewDSL+Subviews.swift
+++ Sources/UIViewKit/UIViewDSL+Subviews.swift
@@ -1,6 +1,6 @@
 //
 //  UIViewDSL+Subviews.swift
-//
+//  UIViewKit
 //
 //  Created by Blazej SLEBODA on 29/09/2023.
 //
@@ -17,8 +17,11 @@ extension UIViewDSL where Self: UIView {
     }
     
     @discardableResult
-    public func ibSubviews(@UIViewBuilder _ content: (UIView) -> [UIView]) -> Self {
-        UIViewDSLEngine.shared.addSubviews(content, to: self)
+    public func ibSubviews(@UIViewBuilder _ content: (Self) -> [UIView]) -> Self {
+        let contentWrapper: (UIView) -> [UIView] = { arg1 in
+            content(arg1 as! Self)
+        }
+        UIViewDSLEngine.shared.addSubviews(contentWrapper, to: self)
         return self
     }
     
@@ -29,8 +32,11 @@ extension UIViewDSL where Self: UIView {
     }
     
     @discardableResult
-    public func callAsFunction(@UIViewBuilder _ content: (UIView) -> [UIView]) -> Self {
-        UIViewDSLEngine.shared.addSubviews(content, to: self)
+    public func callAsFunction(@UIViewBuilder _ content: (Self) -> [UIView]) -> Self {
+        let contentWrapper: (UIView) -> [UIView] = { arg1 in
+            content(arg1 as! Self)
+        }
+        UIViewDSLEngine.shared.addSubviews(contentWrapper, to: self)
         return self
     }
 }
