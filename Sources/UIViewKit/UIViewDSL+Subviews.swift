13b08208aa51033f746549487315ee4991b7bcf4
Blazej SLEBODA <5544365+adobels@users.noreply.github.com>
2023-10-30 08:26:42 +0100

    comment callAsFunction to help Swift type resolver resolve superviews

--- Sources/UIViewKit/UIViewDSL+Subviews.swift
+++ Sources/UIViewKit/UIViewDSL+Subviews.swift
@@ -24,19 +24,19 @@ extension UIViewDSL where Self: UIView {
         UIViewDSLEngine.shared.addSubviews(contentWrapper, to: self)
         return self
     }
-    
-    @discardableResult
-    public func callAsFunction(@UIViewBuilder _ content: () -> [UIView]) -> Self {
-        UIViewDSLEngine.shared.addSubviews(content, to: self)
-        return self
-    }
-    
-    @discardableResult
-    public func callAsFunction(@UIViewBuilder _ content: (Self) -> [UIView]) -> Self {
-        let contentWrapper: (UIView) -> [UIView] = { arg1 in
-            content(arg1 as! Self)
-        }
-        UIViewDSLEngine.shared.addSubviews(contentWrapper, to: self)
-        return self
-    }
+//    
+//    @discardableResult
+//    public func callAsFunction(@UIViewBuilder _ content: () -> [UIView]) -> Self {
+//        UIViewDSLEngine.shared.addSubviews(content, to: self)
+//        return self
+//    }
+//    
+//    @discardableResult
+//    public func callAsFunction(@UIViewBuilder _ content: (Self) -> [UIView]) -> Self {
+//        let contentWrapper: (UIView) -> [UIView] = { arg1 in
+//            content(arg1 as! Self)
+//        }
+//        UIViewDSLEngine.shared.addSubviews(contentWrapper, to: self)
+//        return self
+//    }
 }
