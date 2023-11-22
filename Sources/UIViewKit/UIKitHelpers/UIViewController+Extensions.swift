60c8cf64a2d7b29ebd465bfa943e90962a2ee1f9
Blazej SLEBODA <5544365+adobels@users.noreply.github.com>
2023-11-22 09:45:53 +0100

    swiftlint fix

--- Sources/UIViewKit/UIKitHelpers/UIViewController+Extensions.swift
+++ Sources/UIViewKit/UIKitHelpers/UIViewController+Extensions.swift
@@ -30,10 +30,10 @@ extension UIViewController {
      This method ensures that the view controller hierarchy is updated correctly when a child view controller is removed, maintaining the integrity of the view controller lifecycle which is described [here](https://developer.apple.com/library/archive/featuredarticles/ViewControllerPGforiPhoneOS/ImplementingaContainerViewController.html#//apple_ref/doc/uid/TP40007457-CH11-SW1)
 
      - Parameters:
-        - child: The `UIViewController` instance to be removed from its parent view controller.
+     - child: The `UIViewController` instance to be removed from its parent view controller.
 
      - Important: This method assumes that the `child` view controller is already added to a parent view controller. Calling this method on a view controller that is not a child of any parent will not have any effect.
-    */
+     */
     public func ibRemove(child: UIViewController) {
         child.willMove(toParent: nil)
         child.view.removeFromSuperview()
@@ -46,10 +46,10 @@ extension UIViewController {
      The removal process ensures a safe and clean detachment of the child view controller's view from the view hierarchy which is described [here](https://developer.apple.com/library/archive/featuredarticles/ViewControllerPGforiPhoneOS/ImplementingaContainerViewController.html#//apple_ref/doc/uid/TP40007457-CH11-SW1)
 
      - Parameters:
-        - containerView: The UIView container from which the first subview, if it's a child view controller's view, will be removed.
+     - containerView: The UIView container from which the first subview, if it's a child view controller's view, will be removed.
 
      - Important: This method does not perform any action if `containerView` does not have any subviews or if the first subview is not a child view controller's view.
-    */
+     */
     public func ibRemove(from containerView: UIView) {
         guard let childView = containerView.subviews.first else {
             return
