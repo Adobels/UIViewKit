e2cac6c1f96d3c2ec0700d3ac42ba6e9c47960e7
Blazej SLEBODA <5544365+adobels@users.noreply.github.com>
2024-05-02 23:01:50 +0200

    remove custom P12 import to Keychain app

--- buildxcf.sh
+++ buildxcf.sh
@@ -31,11 +31,5 @@ xcodebuild -create-xcframework \
   -framework "$OUTPUT_DIR/ios_simulator.xcarchive/Products/Library/Frameworks/$PACKAGE_NAME.framework" \
   -output "$OUTPUT_DIR/$XCFRAMEWORK_NAME.xcframework"
 
-# Add SIGN CERTIFICATE TO KEYCHAIN app
-echo "$CERTIFICATE_P12" > certificate.p12
-security import certificate.p12 -k ~/Library/Keychains/login.keychain-db -P "$CERTIFICATE_P12_PASSWORD" -T /usr/bin/codesign -T /usr/bin/security
-security find-certificate -c "iPhone Distribution: Blazej SLEBODA (43VBDTY4E3)" ~/Library/Keychains/login.keychain-db
-rm certificate.p12
-
 # Sign
 codesign -s "iPhone Distribution: Blazej SLEBODA (43VBDTY4E3)" --force --deep -- $OUTPUT_DIR/$XCFRAMEWORK_NAME.xcframework
