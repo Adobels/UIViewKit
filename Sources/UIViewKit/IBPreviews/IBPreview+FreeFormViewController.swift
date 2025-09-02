6eddf321b848b373d89b2dfa87952e8f14561de6
Blazej SLEBODA <5544365+Adobels@users.noreply.github.com>
2025-09-02 09:45:27 +0200

    Add support for snapping to views representing root UIViewControllers of different sizes, such as a small device or a small device with a presented keyboard
    Add support for snapping to views representing root UIViewControllers of different sizes, such as a small device or a small device with a presented keyboard
    
    Rename UIViewControllerFreeFormPreview to FreeForm
    
    Add support for previewing a view in freeform for  view controllers
    
    Delete support for iPhone 11 frame, keep only the small device frame
    
    Keep only FreeForm for UIView and UIViewController
    
    Remove context argument from view and viewcontroller init methods of FreeForm Previews

--- Sources/UIViewKit/IBPreviews/IBPreview+FreeFormViewController.swift
+++ Sources/UIViewKit/IBPreviews/IBPreview+FreeFormViewController.swift
@@ -11,60 +11,167 @@ import SwiftUI
 extension IBPreview {
 
     @available(iOS 13.0, *)
-    public struct FreeFormViewController: UIViewControllerRepresentable {
-
-        private let makeUIViewController: () -> UIViewController
-
-        public init(_ maker: @autoclosure @escaping () -> UIViewController) {
-            makeUIViewController = maker
+    public struct FreeForm<T: UIViewController>: UIViewControllerRepresentable {
+        
+        public typealias UIViewControllerType = UIViewController
+        
+        private let viewControllerMaker: () -> T
+        
+        public init(_ view: UIView) {
+            self.viewControllerMaker = {
+                let vc = T()
+                vc.loadViewIfNeeded()
+                vc.view.ibSubviews {
+                    view.ibAttributes {
+                        $0.ibConstraints(to: vc.view, guide: .view, anchors: .all)
+                    }
+                }
+                return vc
+            }
         }
-
-        public init(_ maker: @escaping () -> UIViewController) {
-            makeUIViewController = maker
+        
+        public init(_ viewController: T) {
+            self.viewControllerMaker = { viewController }
         }
-
+        public init(viewControllerMaker: @escaping () -> T) {
+            self.viewControllerMaker = viewControllerMaker
+        }
+        
         public func makeUIViewController(context: Context) -> UIViewController {
-            let containerVC = ContainerViewController()
-            containerVC.childViewController = makeUIViewController()
-            return containerVC
+            let controller = viewControllerMaker()
+            controller.loadViewIfNeeded()
+            let freeFormContainer = ViewControllerFreeFormContainer()
+            freeFormContainer.loadViewIfNeeded()
+            freeFormContainer.containerView.ibSubviews { superview in
+                controller.view.ibAttributes {
+                    $0.ibConstraints(to: superview, guide: .view, anchors: .all)
+                }
+            }
+            freeFormContainer.addChild(controller)
+            controller.willMove(toParent: freeFormContainer)
+            return freeFormContainer
         }
-
-        public func updateUIViewController(_ uiViewController: UIViewController, context: Context) { }
+        
+        public func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) { }
     }
-}
 
-extension IBPreview {
-
-    final class ContainerViewController: UIViewController {
-
-        var childViewController: UIViewController? {
-            didSet {
-                setupChildViewController()
+    final class ViewControllerFreeFormContainer: UIViewController {
+        var containerView: UIView!
+        var heightConstraint: NSLayoutConstraint!
+        var widthConstraint: NSLayoutConstraint!
+        private var iPhoneSE2FrameView: UIView!
+        private var iPhoneSE2WithKeyboardFrameView: UIView!
+        private var snapToViewFeature: SnapToViewFeature!
+        private let iPhoneSE2Frame = CGRect(origin: .zero, size: .init(width: 375, height: 667))
+        private let iPhoneSE2WithKeyboardFrame = CGRect(origin: .zero, size: .init(width: 375, height: 451))
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            view.backgroundColor = .lightGray
+            view.ibSubviews { superview in
+                UIView().ibOutlet(&iPhoneSE2FrameView).ibSubviews { superview in
+                    UILabel().ibAttributes {
+                        $0.ibConstraints(to: superview, guide: .view, anchors: .left, .bottom, .right)
+                        $0.text = "iPhone SE2"
+                        $0.textAlignment = .center
+                        $0.textColor = .cyan
+                    }
+                }.ibAttributes {
+                    $0.topAnchor.constraint(equalTo: superview.topAnchor)
+                    $0.leadingAnchor.constraint(equalTo: superview.leadingAnchor)
+                    $0.widthAnchor.constraint(equalToConstant: iPhoneSE2Frame.width)
+                    $0.heightAnchor.constraint(equalToConstant: iPhoneSE2Frame.height)
+                    $0.layer.borderColor = UIColor.cyan.cgColor
+                    $0.layer.borderWidth = 2
+                }
+            }
+            view.ibSubviews { superview in
+                UIView().ibOutlet(&iPhoneSE2WithKeyboardFrameView).ibSubviews { superview in
+                    UILabel().ibAttributes {
+                        $0.ibConstraints(to: superview, guide: .view, anchors: .left, .bottom, .right)
+                        $0.text = "iPhone SE2 With Keyboard"
+                        $0.textAlignment = .center
+                        $0.textColor = UIColor.yellow
+                    }
+                }.ibAttributes {
+                    $0.topAnchor.constraint(equalTo: superview.topAnchor)
+                    $0.leadingAnchor.constraint(equalTo: superview.leadingAnchor)
+                    $0.widthAnchor.constraint(equalToConstant: iPhoneSE2WithKeyboardFrame.width)
+                    $0.heightAnchor.constraint(equalToConstant: iPhoneSE2WithKeyboardFrame.height)
+                    $0.layer.borderColor = UIColor.cyan.cgColor
+                    $0.layer.borderWidth = 2
+                }
+            }
+            view.ibSubviews { superview in
+                UIView().ibOutlet(&containerView).ibAttributes {
+                    $0.topAnchor.constraint(equalTo: superview.topAnchor)
+                    $0.leadingAnchor.constraint(equalTo: superview.leadingAnchor)
+                    $0.widthAnchor.constraint(equalToConstant: view.frame.width).ibOutlet(&widthConstraint)
+                    $0.heightAnchor.constraint(equalToConstant: view.frame.height).ibOutlet(&heightConstraint)
+                }
+            }
+            snapToViewFeature = .init(
+                viewToSnap: [
+                    iPhoneSE2WithKeyboardFrameView,
+                    iPhoneSE2FrameView,
+                    view,
+                ],
+                containerView: containerView,
+                controller: self
+            )
+            view.addGestureRecognizer(snapToViewFeature.tapGesture())
+        }
+        
+        override func viewDidAppear(_ animated: Bool) {
+            super.viewDidAppear(animated)
+            workaroundHideKeyboardAndSizeContainerView()
+        }
+        
+        private func workaroundHideKeyboardAndSizeContainerView() {
+            DispatchQueue.main.async {
+                UIView.performWithoutAnimation {
+                    self.view.endEditing(true)
+                }
             }
         }
-
-        private func setupChildViewController() {
-            guard let childVC = childViewController else { return }
-
-            addChild(childVC)
-            view.addSubview(childVC.view)
-
-            childVC.view.frame = .init(origin: .zero, size: view.frame.size)
-
-            childVC.didMove(toParent: self)
-
-            let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:)))
-            view.addGestureRecognizer(panGesture)
+        
+        override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+            super.touchesBegan(touches, with: event)
+            guard let touch = touches.first else { return }
+            let loc = touch.location(in: view)
+            heightConstraint.constant = loc.y
+            widthConstraint.constant = loc.x
         }
+    }
 
-        @objc private func handlePanGesture(_ gesture: UIPanGestureRecognizer) {
-            let translation = gesture.translation(in: view)
-            if let childView = childViewController?.view {
-                let newWidth = max(20, childView.frame.width + translation.x)
-                let newHeight = max(20, childView.frame.height + translation.y)
-                childView.frame.size = CGSize(width: newWidth, height: newHeight)
+    final class SnapToViewFeature {
+        
+        private let viewToSnap: [UIView]
+        private let containerView: UIView
+        private let controller: ViewControllerFreeFormContainer
+        
+        init(viewToSnap: [UIView], containerView: UIView, controller: ViewControllerFreeFormContainer) {
+            self.viewToSnap = viewToSnap
+            self.containerView = containerView
+            self.controller = controller
+        }
+        
+        func tapGesture() -> UIGestureRecognizer {
+            UITapGestureRecognizer(target: self, action: #selector(didTap(gesture:)))
+        }
+        
+        @objc
+        func didTap(gesture: UIGestureRecognizer) {
+            guard let tapGesture = gesture as? UITapGestureRecognizer else { return }
+            for viewToSnap in self.viewToSnap {
+                let loc = tapGesture.location(in: viewToSnap)
+                if viewToSnap.frame.contains(loc) {
+                    controller.widthConstraint.constant = viewToSnap.frame.width
+                    controller.heightConstraint.constant = viewToSnap.frame.height
+                    return
+                }
             }
-            gesture.setTranslation(.zero, in: view)
         }
+        
     }
 }
