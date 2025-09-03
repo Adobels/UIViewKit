b0d7b57e28de95761aa1e7720b136387bf7be61e
Blazej SLEBODA <5544365+Adobels@users.noreply.github.com>
2025-09-03 11:44:04 +0200

    Add frame argument to UIStackView convenience init method

--- Sources/UIViewKit/UIKitExtensions/UIStackView+Extensions.swift
+++ Sources/UIViewKit/UIKitExtensions/UIStackView+Extensions.swift
@@ -9,8 +9,8 @@ import UIKit
 
 extension UIStackView {
     
-    public convenience init(axis: NSLayoutConstraint.Axis, spacing: CGFloat? = nil, alignment: UIStackView.Alignment? = nil, distribution: UIStackView.Distribution? = nil) {
-        self.init()
+    public convenience init(frame: CGRect = .zero, axis: NSLayoutConstraint.Axis, spacing: CGFloat? = nil, alignment: UIStackView.Alignment? = nil, distribution: UIStackView.Distribution? = nil) {
+        self.init(frame: frame)
         self.axis = axis
         if let spacing {
             self.spacing = spacing
